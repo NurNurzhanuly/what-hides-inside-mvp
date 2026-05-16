ac549c89e1b50c8d4b13ff976040beb2d08f56f9
NurNurzhanuly <nur_nurzhanuly@icloud.com>
2026-05-16 05:12:28 +0500

    feat: Update prefabs and scripts for improved gameplay mechanics

--- Assets/Scripts/Environment/Bullet.cs
+++ Assets/Scripts/Environment/Bullet.cs
@@ -1,36 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour
 {
     public float speed = 10f;
-    public float lifeTime = 3f; // Чтобы пуля не летала вечно, если промахнулась
+    public float lifeTime = 3f;
 
     private Rigidbody2D _rb;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
-        // Пуля летит в сторону своего "правого" вектора (убедитесь, что пуля смотрит вправо в префабе)
+        // Если пуля не летит, проверь, не стоит ли у нее Body Type = Kinematic
         _rb.linearVelocity = transform.right * speed;
         
-        // Самоуничтожение, если не попали в цель
         Destroy(gameObject, lifeTime);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Если попали в объект, который можно убить
+        // 1. Сначала проверяем: можем ли мы нанести урон? (Игрок)
         IDamageable damageable = collision.GetComponent<IDamageable>() ?? collision.GetComponentInParent<IDamageable>();
-        
         if (damageable != null)
         {
             damageable.TakeDamage(100f);
+            Destroy(gameObject);
+            return;
         }
 
-        // В любом случае (попали в игрока или в стену) пуля исчезает
+        // Если объект, в который мы попали, является триггером (лестница, зона камеры и т.д.) пуля летит дальше, игнорируя его
+        if (collision.isTrigger)
+        {
+            return; 
+        }
+        
+        // В этом случае пуля должна разбиться.
         Destroy(gameObject);
     }
 }
